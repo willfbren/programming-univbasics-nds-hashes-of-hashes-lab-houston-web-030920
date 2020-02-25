da733d2d834b1e400b1c2dedb02ed3a7baa3887f
Will Brennan <willfbren@gmail.com>
2020-02-25 19:09:19 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -1,4 +1,4 @@
-lear# Build a nested HoH of the classification of biological life on Earth based
+# Build a nested HoH of the classification of biological life on Earth based
 # on these commented-out snippets assigned to PORTION_1-PORTION_4. Return
 # that HoH as the return value for the method "naming_system."
 #
